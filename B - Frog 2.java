c75a03a8356827b60751c4ab104b5dbfd7360912
Sourabh Kanojia <sunnycarlos2303@gmail.com>
2019-09-24 22:55:52 +0530

    Create B - Frog 2.java

---
+++ B - Frog 2.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int[] h=new int[n];
+        for(int i=0;i<n;i++){
+            h[i]=sc.nextInt();
+        }
+        int[] dp=new int[n];
+        dp[0]=0;
+        dp[1]=Math.abs(h[0]-h[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=min(h,dp,i,k);
+        }
+        System.out.println(dp[n-1]);
+    }
+}
